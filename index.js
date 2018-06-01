a623fb7eed148a24b1434994dcfab8ff20e2f955
Christopher Arnold <chrisarnold2010@gmail.com>
2018-06-01 17:33:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const app = "I don't do much."
 
 var kittens = ['Otis', 'Milo', 'Garfield'];
 function destructivelyAppendKitten(kittens, Ralph) {
-  kittens.push(Ralph);
-  return array;
+  return kittens.push(Ralph);
 }
